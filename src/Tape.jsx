6cafd20a5b9381649e23da3bc8be9527c13f7ea7
gajjoshi <gajbjoshi@gmail.com>
2024-04-23 01:58:51 +0530

    csed2

--- src/Tape.jsx
+++ src/Tape.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import golden from './assets/golden.jpg';
 import greenGlitter from './assets/green_glitter.jpg'; // Import the green_glitter image
+import blueGlitter from './assets/blue_glitter.jpg'; // Import the green_glitter image
+
 import photo from './assets/MyC.png';
 import './Tape.css';
 
@@ -17,12 +19,20 @@ export default function Tape() {
     backgroundSize: 'cover',
     backgroundPosition: 'center',
   };
+  const buttonBackgroundStyle2 = {
+    backgroundImage: `url(${blueGlitter})`, // Use green_glitter image for the button background
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  };
 
   return (
     <div style={backgroundImageStyle}>
       <footer className="font-bebas flex flex-col justify-center items-center text-white h-60">
         <a href="https://wa.link/mycircle247" className="button" style={buttonBackgroundStyle}> {/* Apply button background style */}
           <h1 className="text-3xl lg:text-5xl mb-2 text-animation">CLICK NOW FOR WHATSAPP</h1>
+        </a>
+        <a href="https://t.me/mycircle247_book" className="button" style={buttonBackgroundStyle2}> {/* Apply button background style */}
+          <h1 className="text-3xl lg:text-5xl mb-2 text-animation">CLICK NOW FOR TELEGRAM</h1>
         </a>
         <div className="join-with" style={{color:"black"}}>
           <h1 className="text-3xl lg:text-5xl mb-2">JOIN WITH</h1>
